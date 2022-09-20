eb73209f46a523bbc608f4657acbf0cd8f4cba57
Oleksandr Mansarliiskyi <a.mansarliyskiy@gmail.com>
2022-09-20 19:07:51 +0300

    add tree

--- 2-0-data-structures-and-algorithms/2-2-6-binary-search-tree/src/main/java/com/bobocode/cs/RecursiveBinarySearchTree.java
+++ 2-0-data-structures-and-algorithms/2-2-6-binary-search-tree/src/main/java/com/bobocode/cs/RecursiveBinarySearchTree.java
@@ -1,8 +1,8 @@
 package com.bobocode.cs;
 
-import com.bobocode.util.ExerciseNotCompletedException;
-
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 /**
  * {@link RecursiveBinarySearchTree} is an implementation of a {@link BinarySearchTree} that is based on a linked nodes
@@ -18,32 +18,128 @@ import java.util.function.Consumer;
  */
 public class RecursiveBinarySearchTree<T extends Comparable<T>> implements BinarySearchTree<T> {
 
-    public static <T extends Comparable<T>> RecursiveBinarySearchTree<T> of(T... elements) {
-        throw new ExerciseNotCompletedException();
+    private static class Node<T> {
+        T element;
+        Node<T> leftNode;
+        Node<T> rightNode;
+
+        public Node(T element) {
+            this.element = element;
+        }
+
     }
+
+    int size;
+    private Node<T> root;
+
+    public static <T extends Comparable<T>> RecursiveBinarySearchTree<T> of(T... elements) {
+        RecursiveBinarySearchTree<T> recursiveBinarySearchTree = new RecursiveBinarySearchTree<>();
+        Stream.of(elements).forEach(recursiveBinarySearchTree::insert);
+        return recursiveBinarySearchTree;
+    }
+
+    /**
+     * insert an element
+     *
+     * @return true if element did not exist in the tree and was inserted successfully
+     */
 
     @Override
     public boolean insert(T element) {
-        throw new ExerciseNotCompletedException();
+        if (root == null) {
+            root = new Node<>(element);
+            size++;
+            return true;
+        } else {
+            return insert(root, element);
+        }
     }
+
+    private boolean insert(Node<T> current, T element) {
+        if (element.compareTo(current.element) < 0) {
+            if (current.leftNode == null) {
+                current.leftNode = new Node<>(element);
+                size++;
+                return true;
+            } else {
+                return insert(current.leftNode, element);
+            }
+        } else if (element.compareTo(current.element) > 0) {
+            if (current.rightNode == null) {
+                current.rightNode = new Node<>(element);
+                size++;
+                return true;
+            } else {
+                return insert(current.rightNode, element);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @return true if tree contains element
+     */
 
     @Override
     public boolean contains(T element) {
-        throw new ExerciseNotCompletedException();
+        Objects.requireNonNull(element);
+        return contains(root, element);
     }
+
+    private boolean contains(Node<T> current, T element) {
+        if (current == null) {
+            return false;
+        } else if (element.compareTo(current.element) < 0) {
+            return contains(current.leftNode, element);
+        } else if (element.compareTo(current.element) > 0) {
+            return contains(current.rightNode, element);
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @return number of elements in the tree
+     */
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException();
+        return size;
     }
+
+    /**
+     * @return max. number of transition between root node and any other node; 0 - if tree is empty or contains 1 element
+     */
 
     @Override
     public int depth() {
-        throw new ExerciseNotCompletedException();
+        return root != null ? depth(root) - 1 : 0;
     }
+
+    public int depth(Node<T> current) {
+        if (current == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(depth(current.leftNode), depth(current.rightNode));
+        }
+    }
+
+    /**
+     * traverse the tree in element's natural order
+     *
+     * @param consumer accepts ref. to node during traversing
+     */
 
     @Override
     public void inOrderTraversal(Consumer<T> consumer) {
-        throw new ExerciseNotCompletedException();
+        inOrderTraversal(consumer, root);
+    }
+
+    public void inOrderTraversal(Consumer<T> consumer, Node<T> current) {
+        if (current != null) {
+            inOrderTraversal(consumer, current.leftNode);
+            consumer.accept(current.element);
+            inOrderTraversal(consumer, current.rightNode);
+        }
     }
 }
